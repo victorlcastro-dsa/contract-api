725d6cd1f7d095ea442b149d75a2bfedcddc0d1c
Victor L. Castro <victorlcastro.dsa@gmail.com>
2025-01-18 18:16:12 -0300

    Basic Config Abstraction & __init__.py Fix

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,43 +1,29 @@
-from quart import Quart, request, jsonify
-from quart_schema import QuartSchema, validate_request, validate_response, Info, Contact
+from quart import Quart
+from quart_schema import QuartSchema, Info, Contact
 from .database import Database
 from quart_bcrypt import Bcrypt
 from . import routes
 from . import main
+from .config import QuartConfig, BcryptConfig, QuartSchemaConfig
 
 app = Quart(__name__)
-app.config["DEBUG"] = True  # Enable debug mode
-app.config["BCRYPT_LOG_ROUNDS"] = 12  # Security configuration for Bcrypt
+app.config.from_object(QuartConfig)
+app.config.from_object(BcryptConfig)
 
 bcrypt = Bcrypt(app)
 database = Database()
 QuartSchema(app, info=Info(
-    title="contract-api",
-    version="0.1.0",
-    description="The `contract-api` is developed in Python using the Quart framework. It is designed to manage information related to contracts, offering routes for creating, retrieving, updating, and deleting data. The API includes features such as authentication, database integration, and various utility endpoints to handle contract management efficiently.",
+    title=QuartSchemaConfig.TITLE,
+    version=QuartSchemaConfig.VERSION,
+    description=QuartSchemaConfig.DESCRIPTION,
     contact=Contact(
-        email="victorlcastro.dsa@gmail.com",
-        name="Victor L. Castro",
-        url="https://github.com/victorlcastro-dsa"
+        email=QuartSchemaConfig.CONTACT_EMAIL,
+        name=QuartSchemaConfig.CONTACT_NAME,
+        url=QuartSchemaConfig.CONTACT_URL
     ),
-    summary="A comprehensive API for managing contract-related information, built with the Quart framework.",
-    terms_of_service="https://github.com/victorlcastro-dsa/contract-api/blob/main/TERMS.md"
-), tags=[
-    {"name": "Main", "description": "Main routes for the API, including health check and API information."},
-    {"name": "Union", "description": "Management of unions, including creation, retrieval, updating, and deletion of unions."},
-    {"name": "Sei", "description": "Management of SEIs, including creation, retrieval, updating, and deletion of SEIs."},
-    {"name": "Address", "description": "Management of addresses, including creation, retrieval, updating, and deletion of addresses."},
-    {"name": "OrganizationClient", "description": "Management of organization clients, including creation, retrieval, updating, and deletion of organization clients."},
-    {"name": "Contract", "description": "Management of contracts, including creation, retrieval, updating, and deletion of contracts."},
-    {"name": "Attachment", "description": "Management of attachments, including creation, retrieval, updating, and deletion of attachments."},
-    {"name": "GeneralInformation", "description": "Management of general information, including creation, retrieval, updating, and deletion of general information."},
-    {"name": "Item", "description": "Management of items, including creation, retrieval, updating, and deletion of items."},
-    {"name": "Role", "description": "Management of roles, including creation, retrieval, updating, and deletion of roles."},
-    {"name": "Reassignment", "description": "Management of reassignments, including creation, retrieval, updating, and deletion of reassignments."},
-    {"name": "Additional", "description": "Management of additionals, including creation, retrieval, updating, and deletion of additionals."},
-    {"name": "Contact", "description": "Management of contacts, including creation, retrieval, updating, and deletion of contacts."}
-],
-conversion_preference='pydantic')
+    summary=QuartSchemaConfig.SUMMARY,
+    terms_of_service=QuartSchemaConfig.TERMS_OF_SERVICE
+), tags=QuartSchemaConfig.TAGS, conversion_preference=QuartSchemaConfig.CONVERSION_PREFERENCE)
 
 # Registrar os blueprints
 app.register_blueprint(main.main_bp)
---
+++ src/api/config.py
@@ -0,0 +1,35 @@
+class QuartConfig:
+    DEBUG = True  # Enable debug mode
+
+class BcryptConfig:
+    BCRYPT_LOG_ROUNDS = 12  # Security configuration for Bcrypt
+
+class QuartSchemaConfig:
+    CONVERSION_PREFERENCE = 'pydantic'
+    TITLE = "contract-api"
+    VERSION = "0.1.0"
+    DESCRIPTION = (
+        "The `contract-api` is developed in Python using the Quart framework. "
+        "It is designed to manage information related to contracts, offering routes for creating, retrieving, updating, and deleting data. "
+        "The API includes features such as authentication, database integration, and various utility endpoints to handle contract management efficiently."
+    )
+    CONTACT_EMAIL = "victorlcastro.dsa@gmail.com"
+    CONTACT_NAME = "Victor L. Castro"
+    CONTACT_URL = "https://github.com/victorlcastro-dsa"
+    SUMMARY = "A comprehensive API for managing contract-related information, built with the Quart framework."
+    TERMS_OF_SERVICE = "https://github.com/victorlcastro-dsa/contract-api/blob/main/TERMS.md"
+    TAGS = [
+        {"name": "Main", "description": "Main routes for the API, including health check and API information."},
+        {"name": "Union", "description": "Management of unions, including creation, retrieval, updating, and deletion of unions."},
+        {"name": "Sei", "description": "Management of SEIs, including creation, retrieval, updating, and deletion of SEIs."},
+        {"name": "Address", "description": "Management of addresses, including creation, retrieval, updating, and deletion of addresses."},
+        {"name": "OrganizationClient", "description": "Management of organization clients, including creation, retrieval, updating, and deletion of organization clients."},
+        {"name": "Contract", "description": "Management of contracts, including creation, retrieval, updating, and deletion of contracts."},
+        {"name": "Attachment", "description": "Management of attachments, including creation, retrieval, updating, and deletion of attachments."},
+        {"name": "GeneralInformation", "description": "Management of general information, including creation, retrieval, updating, and deletion of general information."},
+        {"name": "Item", "description": "Management of items, including creation, retrieval, updating, and deletion of items."},
+        {"name": "Role", "description": "Management of roles, including creation, retrieval, updating, and deletion of roles."},
+        {"name": "Reassignment", "description": "Management of reassignments, including creation, retrieval, updating, and deletion of reassignments."},
+        {"name": "Additional", "description": "Management of additionals, including creation, retrieval, updating, and deletion of additionals."},
+        {"name": "Contact", "description": "Management of contacts, including creation, retrieval, updating, and deletion of contacts."}
+    ]
