89d321e24634385b6851bfe8a5316084eedebe9b
Victor L. Castro <victorlcastro.dsa@gmail.com>
2025-01-18 22:14:59 -0300

    Database folder refactoring

--- src/api/database/__init__.py
+++ src/api/database/__init__.py
@@ -1,17 +1,1 @@
-from tortoise import Tortoise
-from src.api.database.config import TORTOISE_ORM
-
-class Database:
-    _instance = None
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(Database, cls).__new__(cls)
-        return cls._instance
-
-    async def init_db(self):
-        await Tortoise.init(config=TORTOISE_ORM)
-        await Tortoise.generate_schemas()
-
-    async def close_db(self):
-        await Tortoise.close_connections()
+from .database import Database
